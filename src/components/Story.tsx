ef3579ded0a95bc7cf8beeed4bdf4449d92de0c5
Demir Eren <deren@uwaterloo.ca>
2026-01-23 18:59:29 -0500

    read our story button removed

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -122,16 +122,6 @@ export default function Story() {
               </ul>
             </div>
           </div>
-
-          {/* CTA */}
-          <div className="mt-12">
-            <a href="#" className="btn-secondary">
-              <span>Read our story</span>
-              <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-              </svg>
-            </a>
-          </div>
         </div>
       </div>
     </section>
